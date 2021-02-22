6e5a51a6fff7699069f325a52b186cca5506871a
Oberman <h.i.oberman@uu.nl>
2021-02-22 12:25:17 +0100

    Add outcome to DGM and compute AUC

--- R/DGM.R
+++ R/DGM.R
@@ -1,19 +1,31 @@
 # function to generate data for 4 predictors
-sample_predictors <- function(size){
-S <- matrix(c(1,0,0.1,0.1,
-             0,1,0.3,0.2,
-             0.1,0.3,1,0.5,
-             0.1,0.2,0.5,1), nrow=4, byrow = TRUE)
-dat <- rmvnorm(n = size, sigma = S) %>% data.frame()
-dat <- dat %>% mutate(
-  X1 = get_binary(X1),
-  X2 = get_binary(X2))
-return(dat)
+sample_predictors <- function(size) {
+  S <- matrix(
+    c(1, 0, 0.1, 0.1,
+      0, 1, 0.3, 0.2,
+      0.1, 0.3, 1, 0.5,
+      0.1, 0.2, 0.5, 1),
+    nrow = 4,
+    byrow = TRUE
+  )
+  dat <- rmvnorm(n = size, sigma = S) %>% data.frame()
+  dat <- dat %>% mutate(X1 = get_binary(X1),
+                        X2 = get_binary(X2))
+  return(dat)
 }
 
-# transform continuous into binary data
-get_binary <- function(vec){
-  p <- 1/(1 + exp(-vec))
-  y <- rbinom(n = length(p), size = 1, prob = p)
+# compute outcome from predictors
+compute_outcome <- function(preds) {
+  xb <- -2 + 5 * preds[["X1"]] + 2 * preds[["X2"]] - 0.5 * preds[["X3"]] + 0.1 * preds[["X4"]] + rnorm(n=nrow(preds),sd = 10)
+  y <- get_binary(xb)
+  return(y)
+}
+
+# UTILS: transform continuous into binary data
+get_binary <- function(vec) {
+  p <- 1 / (1 + exp(-vec))
+  y <- rbinom(n = length(p),
+              size = 1,
+              prob = p)
   return(y)
 }
