4b79a825d2f668dea8caa0547c531f4d609908b8
Oberman <h.i.oberman@uu.nl>
2021-03-01 12:19:43 +0100

    Create some univar missingness

--- R/DGM.R
+++ R/DGM.R
@@ -1,33 +1,42 @@
 # function to generate data for 10 continuous predictors and 1 binary outcome
-generate_sample <- function(sample_size, covariance_matrix, interaction = FALSE) {
-  # generate observations for the predictors
-  dat <- mvtnorm::rmvnorm(n = sample_size, sigma = covariance_matrix) 
-  # compute the outcome based on linear function of predictors and error term
-  betas <- runif(nrow(covariance_matrix),-1, 1)
-  lin_pred <- dat %*% betas - 100 + rnorm(sample_size, sd = 100)
-  if(interaction){
-    more_betas <- runif(10,-1, 1)
-    lin_pred <- lin_pred - dat %*% more_betas * dat[,1]
+generate_sample <-
+  function(sample_size,
+           covariance_matrix,
+           interaction = FALSE) {
+    # generate observations for the predictors
+    dat <-
+      mvtnorm::rmvnorm(n = sample_size, sigma = covariance_matrix)
+    # compute the outcome based on linear function of predictors and error term
+    betas <- runif(nrow(covariance_matrix), -1, 1)
+    lin_pred <- dat %*% betas - 100 + rnorm(sample_size, sd = 100)
+    if (interaction) {
+      more_betas <- runif(10, -2, 2)
+      lin_pred <- lin_pred - dat %*% more_betas * dat[, 1]
+    }
+    # convert to probabilities
+    p <- 1 / (1 + exp(-lin_pred))
+    # convert to binary outcome
+    Y <- rbinom(n = length(p),
+                size = 1,
+                prob = p)
+    dat <- dat %>% data.frame() %>% cbind(Y, .)
+    return(dat)
   }
-  # convert to probabilities
-  p <- 1 / (1 + exp(-lin_pred))
-  # convert to binary outcome
-  Y <- rbinom(n = length(p),
-              size = 1,
-              prob = p)
-  dat <- dat %>% data.frame() %>% cbind(Y, .)
-  return(dat)
-}
 
 # function to check the prevalence of the outcome variable and the auc of the prediction model
-check_characteristics <- function(dataset){
+check_characteristics <- function(dataset) {
   # prevalence of the outcome
   prevalence <- mean(dataset$Y)
-  # fit prediction model 
+  # fit prediction model
   mod <- glm(Y ~ ., family = "binomial", data = dataset)
   # c index/auc
-  auc <- pROC::roc(Y~prob, data = cbind(dat, prob=predict(mod)), quiet = TRUE)$auc
+  auc <-
+    pROC::roc(Y ~ prob,
+              data = cbind(dat, prob = predict(mod)),
+              quiet = TRUE)$auc
   # output
-  return(data.frame(prevalence = round(prevalence, 2),
-              auc = round(auc, 2)))
+  return(data.frame(
+    prevalence = round(prevalence, 2),
+    auc = round(auc, 2)
+  ))
 }
