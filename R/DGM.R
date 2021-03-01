7089aa54d6967affbebd9c26d64b6e5df1a2378b
Oberman <h.i.oberman@uu.nl>
2021-03-01 10:53:28 +0100

    Add interaction terms to calculation of the outcome

--- R/DGM.R
+++ R/DGM.R
@@ -1,10 +1,14 @@
 # function to generate data for 10 continuous predictors and 1 binary outcome
-generate_sample <- function(sample_size, covariance_matrix) {
+generate_sample <- function(sample_size, covariance_matrix, interaction = FALSE) {
   # generate observations for the predictors
   dat <- mvtnorm::rmvnorm(n = sample_size, sigma = covariance_matrix) 
   # compute the outcome based on linear function of predictors and error term
   betas <- runif(nrow(covariance_matrix),-1, 1)
   lin_pred <- dat %*% betas - 100 + rnorm(sample_size, sd = 100)
+  if(interaction){
+    more_betas <- runif(10,-1, 1)
+    lin_pred <- lin_pred - dat %*% more_betas * dat[,1]
+  }
   # convert to probabilities
   p <- 1 / (1 + exp(-lin_pred))
   # convert to binary outcome
@@ -15,40 +19,15 @@ generate_sample <- function(sample_size, covariance_matrix) {
   return(dat)
 }
 
-# sample_predictors <- function(size) {
-#   # variance-covariance matrix
-#   S <- matrix(
-#     c(1, 0, 0.1, 0.1,
-#       0, 1, 0.3, 0.2,
-#       0.1, 0.3, 1, 0.5,
-#       0.1, 0.2, 0.5, 1),
-#     nrow = 4,
-#     byrow = TRUE
-#   )
-#   # generate observations
-#   dat <- rmvnorm(n = size, sigma = S) %>% data.frame()
-#   # convert some predictors to binary (see function below)
-#   dat <- dat %>% mutate(X1 = get_binary(X1),
-#                         X2 = get_binary(X2))
-#   return(dat)
-# }
-#
-# # compute outcome from predictors
-# compute_outcome <- function(preds) {
-#   # compute the outcome based on linear function of predictors and error term
-#   xb <- -2 + 5 * preds[["X1"]] + 2 * preds[["X2"]] - 0.5 * preds[["X3"]] + 0.1 * preds[["X4"]] + rnorm(n=nrow(preds),sd = 10)
-#   # convert to binary
-#   y <- get_binary(xb)
-#   return(y)
-# }
-#
-# # UTILS: transform continuous into binary data
-# get_binary <- function(vec) {
-#   # convert to probabilities
-#   p <- 1 / (1 + exp(-vec))
-#   # convert to binary
-#   y <- rbinom(n = length(p),
-#               size = 1,
-#               prob = p)
-#   return(y)
-# }
+# function to check the prevalence of the outcome variable and the auc of the prediction model
+check_characteristics <- function(dataset){
+  # prevalence of the outcome
+  prevalence <- mean(dataset$Y)
+  # fit prediction model 
+  mod <- glm(Y ~ ., family = "binomial", data = dataset)
+  # c index/auc
+  auc <- pROC::roc(Y~prob, data = cbind(dat, prob=predict(mod)))
+  # output
+  return(list(prevalence = round(prevalence, 2),
+              auc = round(auc$auc), 2))
+}
