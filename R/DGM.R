f080a98ed3beb6a1b7fd0d75f31bf9445b67305a
Oberman <h.i.oberman@uu.nl>
2021-03-15 15:26:31 +0100

    Fix the effects of the predictors, interactions, and error term in the calculation of the outcome

--- R/DGM.R
+++ R/DGM.R
@@ -7,11 +7,11 @@ generate_sample <-
     dat <-
       mvtnorm::rmvnorm(n = sample_size, sigma = covariance_matrix)
     # compute the outcome based on linear function of predictors and error term
-    betas <- runif(nrow(covariance_matrix), -1, 1)
-    lin_pred <- dat %*% betas - 20 + rnorm(sample_size, sd = 20)
+    betas <- runif(nrow(covariance_matrix), -10, 10)
+    lin_pred <- dat %*% betas + rnorm(sample_size, sd = 35)
     if (interaction) {
-      more_betas <- runif(10, -2, 2)
-      lin_pred <- lin_pred - dat %*% more_betas * dat[, 1]
+      more_betas <- runif(10, -1, 1)
+      lin_pred <- lin_pred + dat %*% more_betas * dat[, 1]
     }
     # convert to probabilities
     p <- 1 / (1 + exp(-lin_pred))
