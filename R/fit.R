3ee2e3258a908245d490a3599b77213b958ebea5
Oberman <h.i.oberman@uu.nl>
2021-06-21 18:28:11 +0200

    Give model objects consistent naming, before removing auc from trainset functions

--- R/fit.R
+++ R/fit.R
@@ -6,33 +6,33 @@ fit_true <- function(dataset) {
   knots <- quantile(dataset$X10, p = c(0.25, 0.5, 0.75))
   # fit prediction model
   mod <- glm(Y ~ splines::bs(X10)+., family = "binomial", data = dataset)
-  # c index/auc
-  auc <-
-    pROC::roc(Y ~ prob,
-              data = cbind(dataset, prob = predict(mod)),
-              quiet = TRUE)$auc
+  # # c index/auc
+  # auc <-
+  #   pROC::roc(Y ~ prob,
+  #             data = cbind(dataset, prob = predict(mod)),
+  #             quiet = TRUE)$auc
   # output
   return(list(
-    mod = mod,
-    auc = round(auc, 2)
+    mod = mod #,
+    # auc = round(auc, 2)
   ))
 }
 
 # function to fit a box of submodels and calculate the auc 
 fit_sub <- function(dataset, form) {
   # fit prediction models
-  mod <- glm(form, family = "binomial", data = dat)
+  mod <- glm(form, family = "binomial", data = dataset)
   #mod_sub4 <- glm(Y ~ X1 + X2 + X3 + X4, family = "binomial", data = dat)
   #mod_sub2 <- glm(Y ~ X1 + X2, family = "binomial", data = dat)
   # c index/auc
-  auc <-
-    pROC::roc(Y ~ prob,
-              data = cbind(dataset, prob = predict(mod)),
-              quiet = TRUE)$auc
+  # auc <-
+  #   pROC::roc(Y ~ prob,
+  #             data = cbind(dataset, prob = predict(mod)),
+  #             quiet = TRUE)$auc
   # output
   return(list(
-    mod = mod,
-    auc = round(auc, 2)
+    mod = mod #,
+    # auc = round(auc, 2)
   ))
 }
 
@@ -42,14 +42,14 @@ fit_rf_imp <- function(dataset) {
   mod <- ranger::ranger(Y~., data = dataset, num.trees = 100, min.node.size = 10)
   # c index/auc
   # pred_dat <- predict(mod, data = dataset) %>% .$predictions %>% cbind(dataset, prob = .) 
-  auc <-
-    pROC::roc(Y ~ prob,
-              data = cbind(dataset, prob = predict(mod, data = dataset)[["predictions"]]),
-              quiet = TRUE)$auc
+  # auc <-
+  #   pROC::roc(Y ~ prob,
+  #             data = cbind(dataset, prob = predict(mod, data = dataset)[["predictions"]]),
+  #             quiet = TRUE)$auc
   # output
   return(list(
-    mod = mod,
-    auc = round(auc, 2)
+    mod = mod #,
+    # auc = round(auc, 2)
   ))
 }
 
@@ -59,13 +59,13 @@ fit_rf_sub <- function(dataset) {
   mod <- party::cforest(Y~., data=dataset)
   # c index/auc
   # pred_dat <- predict(mod, data = dataset) %>% .$predictions %>% cbind(dataset, prob = .) 
-  auc <-
-    pROC::roc(Y ~ prob,
-              data = cbind(dataset, prob = predict(mod, newdata = dataset)),
-              quiet = TRUE)$auc
+  # auc <-
+  #   pROC::roc(Y ~ prob,
+  #             data = cbind(dataset, prob = predict(mod, newdata = dataset)),
+  #             quiet = TRUE)$auc
   # output
   return(list(
-    mod = mod,
-    auc = round(auc, 2)
+    mod = mod #,
+    # auc = round(auc, 2)
   ))
 }
