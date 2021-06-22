2b818dcd1b73fd9f6dd74d734d511c963d805f71
Oberman <h.i.oberman@uu.nl>
2021-06-22 22:09:44 +0200

    Switch off rf surrogate split model prediction temporarily

--- R/perf.R
+++ R/perf.R
@@ -1,11 +1,11 @@
 # functions for the performance of the different methods
 
 # function for the root mean square error
-RMSE <- function(pred, obs){
+calc_RMSE <- function(pred, obs){
   sqrt(mean((pred - obs)^2))
 }
 
 # function for the brier score (combine with RMSE function??)
-brier <- function(pred, obs){
+calc_brier <- function(pred, obs){
   mean((pred - obs)^2)
 }
