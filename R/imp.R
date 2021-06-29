d1ba66ef6c9224686de76e531abbfe2eaa9a665a
Oberman <h.i.oberman@uu.nl>
2021-06-29 13:00:13 +0200

    Conditional imp function for 1 md pattern finished, now extend to other patterns

--- R/imp.R
+++ R/imp.R
@@ -3,9 +3,12 @@
 # inputs
 means <- colMeans(devset[,-1])
 vcov <- cov(devset[,-1])
-dat <- valset[1,-c(1:2)]
-miss <- which(is.na(dat))
-obs <- which(!is.na(dat))
+
+p <- 6
+# dat <- valset[1,-c(1:2)]
+
+miss <- (11-p):10 #which(is.na(dat))
+obs <-  1:(10-p) #which(!is.na(dat))
 # compute
 B <- vcov[miss, miss]
 C <- vcov[miss, obs, drop=FALSE]
@@ -15,10 +18,20 @@ CDinv <- C %*% solve(D)
 cVar <- B - CDinv %*% t(C)
 
 # this is the part where you need individual observations, not before!
-cMu <- c(means[miss] + CDinv %*% (as.numeric(dat[,obs]) - means[obs]))
+# cMu <- c(means[miss] + CDinv %*% (as.numeric(dat[,obs]) - means[obs]))
+
+##########
+x_obs <- valset[valset$p_miss == p,-c(1:2)]
 
 # dat2 <- valset[3,-c(1:2)]
 # 
-# x_obs <- as.numeric(dat[,obs]) %>% list(as.numeric(dat2[,obs]))
+# x_obs2 <- as.numeric(dat[,obs]) %>% list(as.numeric(dat2[,obs]))
 # 
-# cMu <- c(means[miss] + CDinv %*% (x_obs[[1]] - means[obs]))
+# cMu2 <- purrr::map(1:2, function(i){c(means[miss] + CDinv %*% (x_obs2[[i]] - means[obs])) %>% c(x_obs2[[i]], .)})
+# cMu3 <- purrr::map(x_obs2, function(i){c(means[miss] + CDinv %*% (i - means[obs])) %>% c(i, .)})
+# 
+# rownames(dat2)
+
+cMu <- purrr::map_dfr(1:2, function(i){c(means[miss] + CDinv %*% (as.numeric(x_obs[i, obs]) - means[obs])) %>% 
+    c(rownames(x_obs[i, obs]), x_obs[i, obs], .) %>% 
+    setNames(., c("id", names(x_obs)))})
