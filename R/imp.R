8f65a37b556e24b496607ee27fe6976412a6b86e
Oberman <h.i.oberman@uu.nl>
2021-06-29 13:31:48 +0200

    finished imputation method 1 (conditional mean imputation), now make it work for random draw(s) from the conditional

--- R/imp.R
+++ R/imp.R
@@ -1,10 +1,13 @@
 # functions for imputing the missing values in an incomplete set
 
-# inputs
-means <- colMeans(devset[,-1])
-vcov <- cov(devset[,-1])
+# helper function to compute conditional mean and var for imputation
+comp_cond <- function(devs, vals, p){
 
-p <- 6
+# inputs
+means <- colMeans(devs[,-1])
+vcov <- cov(devs[,-1])
+
+# p <- 6
 # dat <- valset[1,-c(1:2)]
 
 miss <- (11-p):10 #which(is.na(dat))
@@ -15,13 +18,13 @@ C <- vcov[miss, obs, drop=FALSE]
 D <- vcov[obs, obs]
 CDinv <- C %*% solve(D)
 # output
-cVar <- B - CDinv %*% t(C)
+c_var <- B - CDinv %*% t(C)
 
 # this is the part where you need individual observations, not before!
 # cMu <- c(means[miss] + CDinv %*% (as.numeric(dat[,obs]) - means[obs]))
 
 ##########
-x_obs <- valset[valset$p_miss == p,-c(1:2)]
+x_obs <- vals[vals$p_miss == p,-c(1:2)]
 
 # dat2 <- valset[3,-c(1:2)]
 # 
@@ -32,6 +35,19 @@ x_obs <- valset[valset$p_miss == p,-c(1:2)]
 # 
 # rownames(dat2)
 
-cMu <- purrr::map_dfr(1:2, function(i){c(means[miss] + CDinv %*% (as.numeric(x_obs[i, obs]) - means[obs])) %>% 
+c_mu <- purrr::map_dfr(1:nrow(x_obs), function(i){c(means[miss] + CDinv %*% (as.numeric(x_obs[i, obs]) - means[obs])) %>% 
     c(rownames(x_obs[i, obs]), x_obs[i, obs], .) %>% 
     setNames(., c("id", names(x_obs)))})
+
+return(list(cond_means = c_mu, cond_vars = c_var))
+}
+
+a <- comp_cond(devs = devset, vals = valset, p = 4)
+b <- comp_cond(devs = devset, vals = valset, p = 6)
+c <- comp_cond(devs = devset, vals = valset, p = 8)
+
+# imputation method 1: Missing values are imputed by their conditional mean
+imp_cond <- rbind(a$cond_means, b$cond_means, c$cond_means) %>% dplyr::arrange(as.numeric(id))
+
+# imputation method 2: Missing values are imputed by a random draw from their conditional multivariate distribution.
+# mvtnorm::rmvnorm()
