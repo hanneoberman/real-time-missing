c4510a7b76b67012f2da4fc8d60ea79d3a0228b7
Oberman <h.i.oberman@uu.nl>
2021-06-30 11:31:35 +0200

    Finish imputation and prediction for conditional means method, now make it work for draw from conditional

--- R/pred.R
+++ R/pred.R
@@ -1,7 +1,10 @@
 # functions to predict the outcome variable from the incomplete data
 
 # function for the box of submodels method
-pred_sub <- function(dataset, submodel_2, submodel_4, submodel_6) {
+pred_sub <- function(dataset,
+                     submodel_2,
+                     submodel_4,
+                     submodel_6) {
   # add the id number to return in the correct order
   dataset <- cbind(id = 1:nrow(dataset), dataset)
   datasplit <- split(dataset, dataset$p_miss)
@@ -25,13 +28,39 @@ pred_sub <- function(dataset, submodel_2, submodel_4, submodel_6) {
         type = "response",
         terms = c("Y", "id")
       )
-    ) %>% 
+    ) %>%
     data.frame(Y_pred = ., id = as.numeric(names(.))) %>% dplyr::arrange(id)
   # output
-  return(Y_pred[,"Y_pred"])
+  return(Y_pred[, "Y_pred"])
 }
 
 # # function for rf with imputation (missranger)
 # pred_rf_imp <- function(dataset, rfmodel_imp){
 #   missRanger::missRanger(dataset) %>% predict(rfmodel_imp, data = .)
 # }
+
+# function for strategy 1, method 1 (conditional mean imp)
+pred_mean <- function(imp_list) {
+  # predict Y for each md pattern
+  Y_pred <- map_dfr(imp_list, ~ {
+    predict(
+      mod_true$mod,
+      newdata = .x$imp_mean[,-c(11:12)],
+      type = "response",
+      terms = c("Y", "id")
+    ) %>%
+      data.frame(Y_pred = ., id = as.numeric(names(.)))
+  }) %>% dplyr::arrange(id)
+  # output
+  return(Y_pred[, "Y_pred"])
+}
+
+# function for strategy 1, method 2 and 3 (conditional draw imp)
+# a <-imp_all[[1]]$imp_draw[[1]] 
+# b <- a %>% 
+#   predict(
+#   mod_true$mod,
+#   newdata = a[,-c(11:12)],
+#   type = "response",
+#   terms = c("Y", "id")
+# )
