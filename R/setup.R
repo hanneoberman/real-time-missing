682da3ca9cc6b7d20b9c03e5886ace276ad8738d
Oberman <h.i.oberman@uu.nl>
2021-09-21 16:41:54 +0200

    Make the script ready for simulation by adding a setup function and combining the DGM functions

---
+++ R/setup.R
@@ -0,0 +1,19 @@
+# setup simulation study
+# run this once
+
+# packages
+library("dplyr") #for wrangling
+library("purrr") #for vectorization
+library("mvtnorm") #for DGM
+library("mice") #for DGM
+library("party") #for prediction
+library("ranger") #for prediction
+
+# parameters
+set.seed(11)
+n_sim <- 2 #TODO: make this 1000
+n_devset <- 10000
+n_valset <- 20000
+p <- 10
+DGM <- define_DGM(p = 10)
+
