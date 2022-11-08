6baafc9b96a0e100dfa2df5a7751e9472f245b0e
pepijnvink <pepijnvink@gmail.com>
2022-11-08 13:12:28 +0100

    Finish preparetion and add Manuscript text

--- preparation/mimic script.R
+++ preparation/mimic script.R
@@ -9,13 +9,6 @@ library(BRRR)
 source("MIMIC Preparation Function.R")
 
 dat <- mimic_prepare(D = "mimic")
-skrrrahh("snoop")
 
-library(mice)
-library(ggmice)
-dat[, -1] %>% plot_pattern()
-plot_pattern
-missing <- md.pattern(dat[, -1], plot = FALSE)
+saveRDS(dat, "mimic_cleaned.rds")
 
-View(missing)
-nrow(missing)
