6e2a0ea0557ac93c9e7a2b3c1d117f762933580b
hanneoberman <h.i.oberman@uu.nl>
2023-07-25 12:08:33 +0200

    fit log and rf models in train set

--- casestudy.R
+++ casestudy.R
@@ -35,47 +35,46 @@ nmis <- colSums(R)
 R <- matrix(R[, order(nmis)], dim(train))
 pat <- apply(R, 1, function(x)
   paste(as.numeric(x), collapse = ""))
-freq_pat <- names(sort(table(pat), decreasing = TRUE))[1:10]
-train_filter <- train[pat %in% freq_pat, ]
+freq_pat <- names(sort(table(pat), decreasing = TRUE))[1:5]
+train_filter <- cbind(pat, train) %>% .[.$pat %in% freq_pat, ]
+
 # filter out less frequent md patterns in training data
 R <- is.na(test)
 nmis <- colSums(R)
 R <- matrix(R[, order(nmis)], dim(test))
 pat <- apply(R, 1, function(x)
   paste(as.numeric(x), collapse = ""))
-test_filter <- test[pat %in% freq_pat, ]
+test_filter <- cbind(pat, test) %>% .[.$pat %in% freq_pat, ]
 
 # save data
 save(train_filter, file = "./Case study/train_filter.Rdata")
 save(test_filter, file = "./Case study/test_filter.Rdata")
 
-# train_pat <-
-#   split(train, ~ pat) %>% # %>% janitor::remove_empty(., which = "cols")
-#   purrr::map(., function(.x) {
-#     .x[,!apply(is.na(.x), 2, all)]
-#   })
-# names(train_pat)
-
-# run all methods except SS
-means <- colMeans(train[,-1], na.rm = TRUE)
-varcov <- cov(train[,-1], use = "pairwise.complete.obs")
+# split training data by pattern and remove completely missing columns
+train_pat <- split(train_filter, ~ pat) %>% 
+  purrr::map(., function(.x) {
+    .x[,!apply(is.na(.x), 2, all)]
+  })
 
 # PS logistic model
 log_mod <- purrr::map(train_pat, ~ {
   glm(Y ~ .,
       family = "binomial",
-      data = .)
+      data = .x[, -1])
 })
+save(log_mod, file = "./Case study/mod_log.Rdata")
 
 # PS random forest model
 rf_mod <- purrr::map(train_pat, ~ {
-  ranger::ranger(Y ~ ., data = .)
+  ranger::ranger(
+    Y ~ ., 
+    data = .x[, -1])
 })
+save(rf_mod, file = "./Case study/mod_rf.Rdata")
 
 # TODO: think about number of patterns, model conv, runtime, sidenote patterns because of ps method, imp train set too?, test set patterns not in train set
 # TODO: filter 20 most freq patterns
-
-# predict(mod, newdata = test, type = "response")
+# note: the 10 most freqent patterns uncluded patterns with complete separation in the outcome (Y = 0 only), so just 5 patterns were selected
 
 ## imputation methods
 imputations <-
@@ -101,3 +100,11 @@ imputations <-
 # })
 # # run surrogate split model
 # sur_mod <- party::cforest(hospital_expire_flag ~ ., data = train)
+# 
+# # run all methods except SS
+# means <- colMeans(train[,-1], na.rm = TRUE)
+# varcov <- cov(train[,-1], use = "pairwise.complete.obs")
+
+
+# predict(mod, newdata = test, type = "response")
+
