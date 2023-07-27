f804a35083e2b421c59006d3ba38f50888721cb6
hanneoberman <h.i.oberman@uu.nl>
2023-07-27 11:48:23 +0200

    make impute function work outside simulation

--- Case study/R/impute.R
+++ Case study/R/impute.R
@@ -1,36 +1,32 @@
-impute <- function(data, mu, sigma, m) {
-  imp_list <- purrr::map(c(4, 6, 8), function(p) {
+impute <- function(data, means, varcov, m) {
     # process inputs
-    miss <- (nrow(sigma) - p + 1):nrow(sigma)
-    obs <-  1:(nrow(sigma) - p)
-    x_obs <- data[data$p_miss == p,-c(1, 2)]
+    x_obs <- data[, names(means)]
+    miss <- apply(is.na(x_obs), 2, any)
+    obs <-  !miss
     # compute
-    B <- sigma[miss, miss]
-    C <- sigma[miss, obs, drop = FALSE]
-    D <- sigma[obs, obs]
+    B <- varcov[miss, miss]
+    C <- varcov[miss, obs, drop = FALSE]
+    D <- varcov[obs, obs]
     CDinv <- C %*% solve(D)
     # conditionals
     c_var <- B - CDinv %*% t(C)
     c_mu <-
-      purrr::map_dfr(1:nrow(x_obs), function(i) {
-        c(mu[miss] + CDinv %*% (as.numeric(x_obs[i, obs]) - mu[obs])) %>%
-          c(as.numeric(rownames(x_obs[i, obs])), p) %>%
-          setNames(c(paste0("X", miss), "id", "p_miss"))
+      purrr::map_dbl(1:nrow(x_obs), function(i) {
+        c(means[miss] + CDinv %*% (as.numeric(x_obs[i, obs]) - means[obs])) 
       })
     # impute mean
     imp_mean <- cbind(x_obs[, obs], c_mu, draw = NA)
     # impute draw
     imp_draw <-
-      purrr::map_dfr(1:nrow(c_mu), function(i) {
-        MASS::mvrnorm(m, mu = as.numeric(c_mu[i, c(paste0("X", miss))]), Sigma = c_var) %>%
+      purrr::map_dfr(1:length(c_mu), function(i) {
+        MASS::mvrnorm(m, mu = as.numeric(c_mu[i]), Sigma = c_var) %>%
           cbind(x_obs[i, obs],
                 .,
-                c_mu[i, c("id", "p_miss")],
+                c_mu = c_mu[i],
                 draw = 1:nrow(.),
                 row.names = NULL)
       })
-    list(mean = imp_mean, draw = imp_draw)
-  }) %>% setNames(c("miss_4", "miss_6", "miss_8"))
+ 
   # output
   imp_means <- map_dfr(imp_list, "mean") %>% dplyr::arrange(id)
   imp_draws <- map_dfr(imp_list, "draw") %>% dplyr::arrange(id)
